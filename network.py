1956eadc4ec6e6d8fe94e6d5db4bd0a8adb64f51
yemintmint <31719849+codeInMint@users.noreply.github.com>
2020-07-23 01:21:16 +0900

    Update network.py

--- network.py
+++ network.py
@@ -12,7 +12,7 @@ def request(url):
         'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
         'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive',
-        'Host': 'https://www.instagram.com/',
+        'Host': 'instagram.com',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64;x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
     }
