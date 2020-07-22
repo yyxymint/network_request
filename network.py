1e8e2930cd4ca3b75461a25a7cf6a1d01ee5aac6
yemintmint <31719849+codeInMint@users.noreply.github.com>
2020-07-23 01:20:54 +0900

    Create network.py

---
+++ network.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import operator
+import time
+import re
+import os
+
+def request(url):
+    header = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9',
+        'Accept-Encoding': 'gzip,deflate',
+        'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Host': 'https://www.instagram.com/',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64;x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+    }
+    try:
+        url_get = requests.get(url, headers=header)
+    except:
+        url_get = requests.get(url, headers=header)
+    return url_get
+   
+def get_soup(url):
+	recept=request(url)
+	return BeautifulSoup(recept.text, "html.parser")
+  
+soup=get_soup(now_url)
+article_list=soup.find_all('tr',{'class':'ub-content us-post'})
