0423ec89bb7d45b317cf00e4fcd1cb06692e6cbc
yyxymint <31719849+yyxymint@users.noreply.github.com>
2021-05-04 09:03:09 +0900

    Add files via upload

---
+++ a_insta_follower.py
@@ -0,0 +1,18 @@
+
+
+def finder(following,follower):
+	real_following=[]
+	for i in following:
+		t=i.find('님의 프로필 사진')
+		if t>0:
+			real_following.append(i[:t])
+
+	real_follower=[]
+	for i in follower:
+		t=i.find('님의 프로필 사진')
+		if t>0:
+			real_follower.append(i[:t])	
+
+	for i in real_following:
+		if i not in real_follower:
+			print(i)
