0f57a5819052918a411d87616f6a851e6c969f6e
yyxymint <31719849+yyxymint@users.noreply.github.com>
2024-04-17 20:40:33 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,26 +1,10 @@
-
-python_file="tsp.py"
-
-file_name="now_testing.txt"
-
-if [ -f $file_name ]
-then
-    FILESIZE=$(wc -c "$file_name" | awk '{print $1}')
-    echo "file size: $FILESIZE"
-    
-    
-    
-    if [ $FILESIZE -gt 500 ]
-    then
-        echo "large enough. Can't overwrite."
-    else
-        echo "Small txt. START"
-        python -u $python_file > $file_name
-            
-    fi
-    
-else
-    echo "NO such file. START"
-    python -u $python_file > $file_name
-    
-fi
+docker run \
+    --detach \
+    --gpus=all \
+    --restart=always \
+    --name=$(whoami)-jupyterlab \
+    --publish=80 \
+    --publish=6006 \
+    --volume=/home/$(whoami)/notebooks:/notebooks \
+    --volume=/storage/$(whoami):/storage \
+    jonghwanhyeon/jupyterlab
